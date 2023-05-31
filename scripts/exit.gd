f5c1ee9a230aff6a4cfcc3ebe811f9a7d4a4c4fb
bpragasa <bpragasa60@gmail.com>
2023-05-31 15:35:08 -0700

    added shopkeeper w/ idle and offer animations

--- scripts/exit.gd
+++ scripts/exit.gd
@@ -3,4 +3,4 @@ extends Area2D
 @export var next_scene: String
 
 func _on_body_entered(body: Node2D) -> void:
-    get_tree().change_scene_to_file(next_scene)
+	get_tree().change_scene_to_file(next_scene)
