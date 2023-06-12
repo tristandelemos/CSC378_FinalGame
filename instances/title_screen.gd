2ff76358f77ecb374af4ca16c377736702f87c31
tristandelemos <tjdelemos@gmail.com>
2023-06-11 18:18:06 -0700

    added controls screen and backstory

--- instances/title_screen.gd
+++ instances/title_screen.gd
@@ -2,9 +2,12 @@ extends Control
 
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://levels/overworld.tscn")
-
+	get_tree().change_scene_to_file("res://instances/backstory.tscn")
 
 
 func _on_credits_pressed():
 	get_tree().change_scene_to_file("res://instances/credits.tscn")
+
+
+func _on_controls_pressed():
+	get_tree().change_scene_to_file("res://instances/controls_screen.tscn")
